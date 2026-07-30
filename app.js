6a4ff95f914e558d8f9b5d44cc23371c0d87e2da
kidsman9049 <53301755+kidsman9049@users.noreply.github.com>
2026-07-30 22:31:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const defaultProducts = [
     name: "磁吸金刚网纱窗网自装窗户防蚊家用自粘式隐形定制免打孔沙窗门帘",
     detail: "70x120cm;【便捷小窗口+赠送专利安全锁扣】金刚网灰纱+灰框",
     image: "https://gw.alicdn.com/bao/uploaded/i2/728381129/O1CN01xX9cqN1KD80Y4A6rC_!!728381129.jpg_.webp",
-    trackingNo: "UPS55667788",
+    trackingNo: "",
     quantity: 2,
     arrived: false
   },
@@ -50,7 +50,7 @@ const defaultProducts = [
     name: "磁吸金刚网纱窗网自装窗户防蚊家用自粘式隐形定制免打孔沙窗门帘",
     detail: "70x120cm;【2026新款】专利安全锁扣-金刚网灰纱+白框",
     image: "https://gw.alicdn.com/bao/uploaded/i1/728381129/O1CN01P4Szqx1KD86IzIC7f_!!728381129.jpg_.webp",
-    trackingNo: "UPS55667788",
+    trackingNo: "",
     quantity: 2,
     arrived: false
   }
